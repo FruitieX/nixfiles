2cc63d27d5dc1df17afebc800534e6a05f8562df
Rasmus Eskola <fruitiex@gmail.com>
2019-05-07 08:03:35 +0300

    Update vscode extensions

--- configs/vscode-extensions.nix
+++ configs/vscode-extensions.nix
@@ -1,11 +1,5 @@
 { extensions = [
   {
-    name = "Nix";
-    publisher = "bbenoist";
-    version = "1.0.1";
-    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
-  }
-  {
     name = "bracket-pair-colorizer";
     publisher = "CoenraadS";
     version = "1.0.61";
@@ -18,10 +12,28 @@
     sha256 = "1dy7rf9w1mvk65fmxbvhbi5pf6cw2lwi07yhafq9x20c36g6dwyz";
   }
   {
+    name = "vscodeintellicode";
+    publisher = "VisualStudioExptTeam";
+    version = "1.1.6";
+    sha256 = "1jblviv14aijksz2a1cansbjjbay6m1xz9igj9q8g70s0z4v5080";
+  }
+  {
+    name = "Nix";
+    publisher = "bbenoist";
+    version = "1.0.1";
+    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+  {
+    name = "gitlens";
+    publisher = "eamodio";
+    version = "9.7.1";
+    sha256 = "08yg79gxqbxnfimfd6wyv2yvx686578757h8vm0lb9crfzb725r0";
+  }
+  {
     name = "prettier-vscode";
     publisher = "esbenp";
-    version = "1.8.1";
-    sha256 = "0qcm2784n9qc4p77my1kwqrswpji7bp895ay17yzs5g84cj010ln";
+    version = "1.9.0";
+    sha256 = "1an9dlkicj1s0ffy9l9jdvzpcdl0slvln9k05rd6l8g42ri9fp49";
   }
   {
     name = "vscode-styled-components";
@@ -36,28 +48,22 @@
     sha256 = "155frrf8fs0c6sgs532cxgwvxzinkgg4k0ywsbl7zzjip8qqmm0g";
   }
   {
+    name = "debugger-for-chrome";
+    publisher = "msjsdiag";
+    version = "4.11.3";
+    sha256 = "1i5skl12pdd1f5diday0prihdd99kdvcv3www3zrkpvxkpyp8v9a";
+  }
+  {
     name = "color-highlight";
     publisher = "naumovs";
     version = "2.3.0";
     sha256 = "1syzf43ws343z911fnhrlbzbx70gdn930q67yqkf6g0mj8lf2za2";
   }
   {
-    name = "vscodeintellicode";
-    publisher = "VisualStudioExptTeam";
-    version = "1.1.5";
-    sha256 = "12d6s7kfk4102gnfb2rk1sb00p3ql9xy86jykz562jlb20frp9s1";
-  }
-  {
-    name = "vim";
-    publisher = "vscodevim";
-    version = "1.4.0";
-    sha256 = "0vfhvsp485rgik3pjzbpnc4jxrjpiykynl563a16rlz8h85x2m4f";
-  }
-  {
-    name = "vscode-import-cost";
-    publisher = "wix";
-    version = "2.12.0";
-    sha256 = "1g6k8fxfa49ky8v3l5n6l7p6gnjf9sdd56crcj33p08gb8pyy86l";
+    name = "vscode-jest";
+    publisher = "Orta";
+    version = "2.9.2";
+    sha256 = "05y1hk0j001ibyzd76689fc4x8yfds0w5qwq87jdbr3xyfa4h090";
   }
   {
     name = "shader";
@@ -66,22 +72,22 @@
     sha256 = "0m5ld6l5sizqbj2k6754n2hn50ygajy464qz66dar2n3nhvjdv9n";
   }
   {
-    name = "debugger-for-chrome";
-    publisher = "msjsdiag";
-    version = "4.11.3";
-    sha256 = "1i5skl12pdd1f5diday0prihdd99kdvcv3www3zrkpvxkpyp8v9a";
+    name = "code-spell-checker";
+    publisher = "streetsidesoftware";
+    version = "1.7.9";
+    sha256 = "15p80fcslb4d7abm8gc0n0sdifzv18cm50l0g7zjz8c9dpfbkkcg";
   }
   {
-    name = "vscode-jest";
-    publisher = "orta";
-    version = "2.9.2";
-    sha256 = "05y1hk0j001ibyzd76689fc4x8yfds0w5qwq87jdbr3xyfa4h090";
+    name = "vim";
+    publisher = "vscodevim";
+    version = "1.7.1";
+    sha256 = "0zmlwsb5fs7nsiv0yqyr6156y0ziajwji7f7c1cw1lbgfz1s5ph7";
   }
   {
-    name = "gitlens";
-    publisher = "eamodio";
-    version = "9.7.0";
-    sha256 = "1czypyqwbi19vbqamnvhvranyx1am5b7jrb8l6k8c3826wbs9wrj";
+    name = "vscode-import-cost";
+    publisher = "wix";
+    version = "2.12.0";
+    sha256 = "1g6k8fxfa49ky8v3l5n6l7p6gnjf9sdd56crcj33p08gb8pyy86l";
   }
 ];
 }
