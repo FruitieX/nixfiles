cefb276d3610ed3ef694be74f224e16732b8172c
Rasmus Eskola <fruitiex@gmail.com>
2019-10-02 16:54:49 +0300

    yolo

--- common.nix
+++ common.nix
@@ -94,7 +94,7 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.09"; # Did you read the comment?
+  system.stateVersion = "19.03"; # Did you read the comment?
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.flags = [ "-I" "nixos-config=/home/${user}/nixfiles/configuration.nix" ];
