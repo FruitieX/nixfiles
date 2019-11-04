9b0d0599868415c47c179ae44106933cf3193e43
Rasmus Eskola <fruitiex@gmail.com>
2019-11-04 09:37:32 +0200

    tamarillo host

---
+++ hosts/tamarillo/default.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, user, ... }:
+
+{
+  imports =
+    [
+      ../../client.nix
+    ];
+
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/disk/by-uuid/8f6522ac-418b-43a2-b006-a61ff4113568";
+      preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.displayManager.gdm.wayland = false;
+
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.docker.enable = true;
+}
