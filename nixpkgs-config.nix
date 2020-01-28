d26d3aec98eae48e8ccd463da9915f9f350d7df5
Rasmus Eskola <fruitiex@gmail.com>
2020-01-28 13:36:52 +0200

    nixpkgsFmt

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -14,30 +14,30 @@
 
       # Unstable package set
       # View last updated time at: https://howoldis.herokuapp.com
-      # unstable = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {
-      #   config = self.config;
-      # };
+      unstable = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {
+        config = self.config;
+      };
 
       # Unstable-small package set
       # Contains more recent packages, but larger chance of having to build from source.
       # This is fine for binary packages though, which build fast anyway.
-      # unstable-small = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable-small.tar.gz) {
-      #   config = self.config;
-      # };
+      unstable-small = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable-small.tar.gz) {
+        config = self.config;
+      };
 
       # Channel based
 
       # unstable package set, install by running:
       # sudo nix-channel --add https://nixos.org/channels/nixos-unstable unstable
-      unstable = import <unstable> {
-        config = self.config;
-      };
+      # unstable = import <unstable> {
+      #   config = self.config;
+      # };
 
       # unstable-small package set, install by running:
       # sudo nix-channel --add https://nixos.org/channels/nixos-unstable-small unstable-small
-      unstable-small = import <unstable-small> {
-        config = self.config;
-      };
+      # unstable-small = import <unstable-small> {
+      #   config = self.config;
+      # };
 
       # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
       #master = import (fetchFromGitHub {
@@ -64,6 +64,15 @@
           sha256 = "0g7c4p8w738s0rnn1pbr39g0qv0jp6rwbdzimmb28g9g82mnc5v6";
         }
       ) { pkgs = master; };
+
+      nixpkgsFmt = import (
+        pkgs.fetchFromGitHub {
+          owner = "justinwoo";
+          repo = "my-nixpkgs-fmt";
+          rev = "4496a67817e3dc2f40c60db971a1d503b353415c";
+          sha256 = "1z9k1jlbqlfb2wgqlvpqvlrxwpz3j9syinx6f8705z34q1yjnxnb";
+        }
+      ) {};
 
       systemToolsEnv = with super; buildEnv {
         name = "systemToolsEnv";
@@ -119,11 +128,20 @@
           nix-prefetch-git
           lorri
           updatePrefetch
+          nixpkgsFmt
 
           # Development
           # unstable-small.nodejs-11_x
           # (unstable-small.yarn.override { nodejs = unstable-small.nodejs-11_x; })
           postgresql_11
+        ];
+      };
+
+      otherOsEnv = with super; buildEnv {
+        name = "otherOsEnv";
+        paths = [
+          # (unstable.fish.override (import ./configs/fish-config.nix { inherit pkgs; }))
+          # (unstable.tmux.override (import ./configs/tmux-config.nix { inherit pkgs; }))
         ];
       };
 
