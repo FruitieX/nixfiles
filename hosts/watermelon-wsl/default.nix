c8a2c1f4f25d22adabba732f301969dc0b369006
Rasmus Eskola <fruitiex@gmail.com>
2020-09-30 15:19:03 +0300

    remove postgresql from watermelon-wsl

--- hosts/watermelon-wsl/default.nix
+++ hosts/watermelon-wsl/default.nix
@@ -5,14 +5,14 @@
   virtualisation.docker.enable = true;
 
   # PostgreSQL
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_11;
-    authentication = lib.mkForce ''
-      # TYPE  DATABASE        USER            ADDRESS                 METHOD
-      local   all             all                                     trust
-      host    all             all             127.0.0.1/32            trust
-      host    all             all             ::1/128                 trust
-    '';
-  };
+  # services.postgresql = {
+  #   enable = true;
+  #   package = pkgs.postgresql_11;
+  #   authentication = lib.mkForce ''
+  #     # TYPE  DATABASE        USER            ADDRESS                 METHOD
+  #     local   all             all                                     trust
+  #     host    all             all             127.0.0.1/32            trust
+  #     host    all             all             ::1/128                 trust
+  #   '';
+  # };
 }
