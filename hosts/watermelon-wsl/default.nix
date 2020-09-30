5e103d76a1b69e93b1ce2bb164e47312eddedfca
Rasmus Eskola <fruitiex@gmail.com>
2020-09-30 11:44:41 +0000

    watermelon-wsl host

---
+++ hosts/watermelon-wsl/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, user, ... }:
+
+{
+  # virtualisation.virtualbox.host.enable = true;
+  virtualisation.docker.enable = true;
+
+  # PostgreSQL
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_11;
+    authentication = lib.mkForce ''
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+      local   all             all                                     trust
+      host    all             all             127.0.0.1/32            trust
+      host    all             all             ::1/128                 trust
+    '';
+  };
+}
